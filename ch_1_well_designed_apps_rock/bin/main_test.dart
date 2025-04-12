ac64d531d7e0998ce4b73c74d48f60d4c04fddbf
mahmoudFawzee <mahmoudfawzee1511@gmail.com>
2025-04-12 14:51:44 +0200

    add mandolin instrument

--- ch_1_well_designed_apps_rock/bin/main_test.dart
+++ ch_1_well_designed_apps_rock/bin/main_test.dart
@@ -1,57 +1,69 @@
 import 'package:ch_1_well_designed_apps_rock/enum/enums.dart';
-import 'package:ch_1_well_designed_apps_rock/models/builder.dart';
-import 'package:ch_1_well_designed_apps_rock/models/guitar_spec.dart';
-import 'package:ch_1_well_designed_apps_rock/models/type.dart';
-import 'package:ch_1_well_designed_apps_rock/models/wood.dart';
+import 'package:ch_1_well_designed_apps_rock/models%20copy/builder/builder.dart';
+import 'package:ch_1_well_designed_apps_rock/models%20copy/guitar_spec.dart';
+import 'package:ch_1_well_designed_apps_rock/models%20copy/type/type.dart';
+import 'package:ch_1_well_designed_apps_rock/models%20copy/wood/wood.dart';
 import 'package:ch_1_well_designed_apps_rock/services/inventory.dart';
 
 void main(List<String> arguments) {
   //test app
   final inventory = Inventory();
   inventory.addGuitar(
+    model: '569ddd',
+    numStrings: '5',
     serialNumber: 'lll2000',
     price: 50.2,
-    topWood: GuitarWood(WoodEnum.adirondack),
-    backWood: GuitarWood(WoodEnum.alder),
-    builder: GuitarBuilder(BuilderEnum.collings),
-    type: GuitarType(TypeEnum.electric),
+    topWood: Wood(WoodEnum.adirondack),
+    backWood: Wood(WoodEnum.alder),
+    builder: InstrumentBuilder(BuilderEnum.collings),
+    type: InstrumentType(TypeEnum.electric),
   );
   inventory.addGuitar(
+    model: 'gg455',
+    numStrings: '9',
     serialNumber: 'll2001',
     price: 150.2,
-    topWood: GuitarWood(WoodEnum.brazilianRoseWood),
-    backWood: GuitarWood(WoodEnum.indianRoseWood),
-    builder: GuitarBuilder(BuilderEnum.fender),
-    type: GuitarType(TypeEnum.electric),
+    topWood: Wood(WoodEnum.brazilianRoseWood),
+    backWood: Wood(WoodEnum.indianRoseWood),
+    builder: InstrumentBuilder(BuilderEnum.fender),
+    type: InstrumentType(TypeEnum.electric),
   );
   inventory.addGuitar(
+    model: 'yoo88',
+    numStrings: '8',
     serialNumber: 'lkl1200',
     price: 200.2,
-    topWood: GuitarWood(WoodEnum.sitka),
-    backWood: GuitarWood(WoodEnum.maple),
-    builder: GuitarBuilder(BuilderEnum.gibson),
-    type: GuitarType(TypeEnum.acoustic),
+    topWood: Wood(WoodEnum.sitka),
+    backWood: Wood(WoodEnum.maple),
+    builder: InstrumentBuilder(BuilderEnum.gibson),
+    type: InstrumentType(TypeEnum.acoustic),
   );
+
   inventory.addGuitar(
     serialNumber: 'kmm130',
     price: 150.2,
-    topWood: GuitarWood(WoodEnum.sitka),
-    backWood: GuitarWood(WoodEnum.maple),
-    builder: GuitarBuilder(BuilderEnum.gibson),
-    type: GuitarType(TypeEnum.acoustic),
+    model: 'xor445',
+    numStrings: '12',
+    topWood: Wood(WoodEnum.sitka),
+    backWood: Wood(WoodEnum.maple),
+    builder: InstrumentBuilder(BuilderEnum.gibson),
+    type: InstrumentType(TypeEnum.acoustic),
   );
 
-  final GuitarSpec requestedGuitarSpec = GuitarSpec(
-    topWood: GuitarWood(WoodEnum.sitka),
-    backWood: GuitarWood(WoodEnum.maple),
-    builder: GuitarBuilder(BuilderEnum.gibson),
-    type: GuitarType(TypeEnum.acoustic),
+  final requestedGuitarSpec = GuitarSpec(
+    model: 'xor445',
+    numStrings: '12',
+    topWood: Wood(WoodEnum.sitka),
+    backWood: Wood(WoodEnum.maple),
+    builder: InstrumentBuilder(BuilderEnum.gibson),
+    type: InstrumentType(TypeEnum.acoustic),
   );
+  
   final searchedGuitars = inventory.search(
     guitarSpec: requestedGuitarSpec,
   );
   inventory.showNumberOfGuitarsInInventory();
-  print('number of guitars : ${searchedGuitars.length}');
+  print('found guitars : ${searchedGuitars.length}');
   for (var guitar in searchedGuitars) {
     print(guitar.toString());
   }
